5158064367b0e5c28bbdac26e16f46eaf72feba3
joaoedson <joaoedson.dantas@outlook.com>
2023-12-14 23:30:19 -0300

    Created the module, component and route of the dashboard functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
+import { DashboardHomeComponent } from './modules/dashboard/page/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardHomeComponent,
   },
 ];
 
---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,12 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+
+@NgModule({
+  declarations: [
+    DashboardHomeComponent
+  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+})
+export class DashboardModule {}
