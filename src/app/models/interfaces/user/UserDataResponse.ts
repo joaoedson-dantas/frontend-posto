23e1ba7fb90793c60e258623438395da0a8df5e1
joaoedson <joaoedson.dantas@outlook.com>
2023-12-14 21:43:17 -0300

    added user creation service

--- src/app/models/interfaces/user/UserDataResponse.ts
+++ src/app/models/interfaces/user/UserDataResponse.ts
@@ -2,5 +2,4 @@ export interface UserDataResponse {
   id: number;
   name: string;
   login: string;
-  password_hash: string;
 }
---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+import { UserDataRequest } from '../../models/interfaces/user/UserDataRequest';
+import { UserDataResponse } from '../../models/interfaces/user/UserDataResponse';
+import { AuthUserDataRequest } from '../../models/interfaces/user/auth/AuthUserDataRequest';
+import { AuthUserDataResponse } from '../../models/interfaces/user/auth/AuthUserDataResponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private API_URL = environment.API_URL;
+  constructor(private http: HttpClient) {}
+
+  /* Service create user */
+  signupUser(requestData: UserDataRequest): Observable<UserDataResponse> {
+    return this.http.post<UserDataResponse>(
+      `${this.API_URL}/users`,
+      requestData
+    );
+  }
+
+  /* Service auth user */
+  authUser(requestData: AuthUserDataRequest): Observable<AuthUserDataResponse> {
+    return this.http.post<AuthUserDataResponse>(
+      `${this.API_URL}/auth/user`,
+      requestData
+    );
+  }
+}
