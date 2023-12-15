cb0f2c4ff02d81419350077eb986da7db8ab0ea3
joaoedson <joaoedson.dantas@outlook.com>
2023-12-14 23:49:20 -0300

    Implemented the service method that checks whether the user is logged into the application

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -7,13 +7,14 @@ import { UserDataRequest } from '../../models/interfaces/user/UserDataRequest';
 import { UserDataResponse } from '../../models/interfaces/user/UserDataResponse';
 import { AuthUserDataRequest } from '../../models/interfaces/user/auth/AuthUserDataRequest';
 import { AuthUserDataResponse } from '../../models/interfaces/user/auth/AuthUserDataResponse';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   private API_URL = environment.API_URL;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
   /* Service create user */
   signupUser(requestData: UserDataRequest): Observable<UserDataResponse> {
@@ -29,5 +30,11 @@ export class UserService {
       `${this.API_URL}/auth/user`,
       requestData
     );
+  }
+
+  /* Check logged in user */
+  isLoggedIn(): boolean {
+    const JWT_TOKEN = this.cookieService.get('USER_INFO');
+    return JWT_TOKEN ? true : false;
   }
 }
