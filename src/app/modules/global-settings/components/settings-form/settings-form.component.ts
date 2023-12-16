037f1ab9c9ec4d61318e60827efebdeb0f757daf
joaoedson <joaoedson.dantas@outlook.com>
2023-12-16 20:39:00 -0300

    feat: added method of editing a configuration

--- src/app/modules/global-settings/components/settings-form/settings-form.component.ts
+++ src/app/modules/global-settings/components/settings-form/settings-form.component.ts
@@ -1,3 +1,4 @@
+import { GlobalSettingsDataTransferService } from './../../../../shared/services/globalSettings/global-settings-data-transfer.service';
 import { GlobalSettingsService } from './../../../../services/GlobalSettings/global-settings.service';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
@@ -5,6 +6,9 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { CreateSettingRequest } from '../../../../models/interfaces/GlobalSettings/request/CreateSettingRequest';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { EventAction } from '../../../../models/interfaces/GlobalSettings/event/EventAction';
+import { GlobalSettingsDataResponse } from '../../../../models/interfaces/GlobalSettings/response/GlobalSettingsDataResponse';
 
 @Component({
   selector: 'app-settings-form',
@@ -13,11 +17,23 @@ import { CreateSettingRequest } from '../../../../models/interfaces/GlobalSettin
 })
 export class SettingsFormComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject();
+  private settingAction!: {
+    event: EventAction;
+    settingData: Array<GlobalSettingsDataResponse>;
+  };
+
+  public settingSelectedData!: GlobalSettingsDataResponse;
+  public settingData!: Array<GlobalSettingsDataResponse>;
 
   // adicioando o formuário reativo
 
   public createSettingForm = this.formBuilder.group({
     key: ['', Validators.required],
+    label: ['', Validators.required],
+    value: ['', Validators.required],
+  });
+
+  public editSettingForm = this.formBuilder.group({
     label: ['', Validators.required],
     value: ['', Validators.required],
   });
@@ -57,14 +73,61 @@ export class SettingsFormComponent implements OnInit, OnDestroy {
     this.createSettingForm.reset();
   }
 
+  handleSubmiteEditSetting(): void {}
+
   constructor(
     private formBuilder: FormBuilder,
     private messageService: MessageService,
+    private globalSettingsDataTransferService: GlobalSettingsDataTransferService,
     private router: Router,
-    private globalSettingsService: GlobalSettingsService
+    private globalSettingsService: GlobalSettingsService,
+    private ref: DynamicDialogConfig
   ) {}
 
+  // metodo responsável por buscar os dados dos produtos selecionados :
+  getSettingSelectedData(settingId: number): void {
+    const allSettings = this.settingAction?.settingData;
+    console.log('testando o all settings ', allSettings.length);
+
+    if (allSettings.length > 0) {
+      const settingFiltered = allSettings.filter(
+        (element) => element?.id === settingId
+      );
+
+      if (settingFiltered) {
+        this.settingSelectedData = settingFiltered[0];
+
+        // setar um valor existente no formulári vazio
+        this.editSettingForm.setValue({
+          label: this.settingSelectedData?.label,
+          value: this.settingSelectedData?.value,
+        });
+      }
+    }
+  }
+
+  // metodo responsável por buscar os dados que estão armazenados em memoria vindos de outra tela
+  getSettingData(): void {
+    // buscando os dados na api
+    this.globalSettingsService
+      .getAllGlobalSettings()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            // persistindo os dados em memoria e no hotObservable
+            this.settingData = response;
+            this.settingData &&
+              this.globalSettingsDataTransferService.setGlobalSettingsDatas(
+                this.settingData
+              );
+          }
+        },
+      });
+  }
+
   ngOnInit(): void {
+    this.settingAction = this.ref.data;
     throw new Error('Method not implemented.');
   }
 
